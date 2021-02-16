a3b187fa0a4fb004d1e40edb126a13c6cdd92a19
Muhammad Faizan <faizansohail077@gmail.com>
2021-02-16 20:03:18 +0500

    validation completed

--- src/Screens/Checkout/CheckoutRight.js
+++ src/Screens/Checkout/CheckoutRight.js
@@ -3,15 +3,29 @@ import { useSelector } from 'react-redux'
 import db from '../../firebase'
 
 const CheckoutRight = () => {
+
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
-    const [number, setNumber] = useState(0)
+    const [number, setNumber] = useState()
     const [address, setAddress] = useState('')
     const [payment, setPayment] = useState('')
     const [orderType, setOrderType] = useState('')
     const { products, user } = useSelector(state => state.CartReducer)
+    let products2 = products
+    console.log('rhis is product 2', products2)
     const [count, setCount] = useState(0)
+    const [error, setError] = useState('')
+    const [nameError, setNameError] = useState('')
+    const [emailError, setEmailError] = useState('')
+    const [numberError, setNumberError] = useState('')
+    const [addressError, setAddressError] = useState('')
+    const [paymentError, setPaymentError] = useState('')
+    const [orderTypeError, setOrderTypeError] = useState('')
+
+
+
+
 
     useEffect(() => {
         total()
@@ -24,29 +38,79 @@ const CheckoutRight = () => {
         setCount(count1)
     }
 
+    const first = (e) => {
+        firstName?.trim()
+        setFirstName(e.target.value)
+    }
+    const last = (e) => {
+        lastName.trim()
+        setLastName(e.target.value)
+    }
+    console.log('this is first name', firstName)
 
+
+    function empty() {
+        products2 = []
+    }
 
     const save = (e) => {
         e.preventDefault()
-        db.collection('users').doc(user?.id).collection('order').doc().set({
-            productList: products,
-            name: firstName + lastName,
-            number: number,
-            email: email,
-            address: address,
-            paymentMethod: payment,
-            orderType: orderType,
-            status: 'pending',
-            totalPrice: count
-        })
-        setFirstName('')
-        setLastName('')
-        setEmail('')
-        setNumber('')
-        setAddress('')
-        setPayment('')
-        setOrderType('')
+        if (!products2.length) {
+            return alert('Add Item To Proceed')
+        }
+        let fName = firstName.trim()
+        if (!fName) {
+            setTimeout(() => {
+                setNameError('')
+            }, 3000)
+            return setNameError('Enter Valid Name')
+        }
+        let lName = lastName.trim()
+        if (!lName) {
+            setTimeout(() => {
+                setNameError('')
+            }, 3000)
+            return setNameError('Enter Valid Name')
+        }
+        if (!number || number.length < 10) {
+            setTimeout(() => {
+                setNumberError('')
+            }, 3000)
+            return setNumberError('Invalid or less number')
+        }
 
+        if (!payment) {
+            return setPaymentError('Enter Payment Type')
+        }
+
+        if (!orderType) {
+            return setOrderTypeError('Select Order type')
+        }
+        else {
+            db.collection('users').doc(user?.id).collection('order').doc().set({
+                productList: products,
+                name: firstName + ' ' + lastName,
+                number: number,
+                email: email,
+                address: address,
+                paymentMethod: payment,
+                orderType: orderType,
+                status: 'pending',
+                totalPrice: count
+            })
+            empty()
+
+            setFirstName('')
+            setLastName('')
+            setEmail('')
+            setNumber('')
+            setAddress('')
+            setPayment('')
+            setOrderType('')
+            setError('')
+            setPaymentError('')
+            setOrderTypeError('Select Order type')
+        }
     }
     return (
         <>
@@ -54,45 +118,35 @@ const CheckoutRight = () => {
                 <div className="row">
                     <div className="col-md-8 order-md-1">
                         <h4 className="mb-3">Billing address</h4>
-                        <form className="needs-validation" novalidate>
+                        <form onSubmit={save} className="needs-validation" novalidate>
                             <div className="row">
                                 <div className="col-md-6 mb-3">
                                     <label htmlFor="firstName">First name</label>
-                                    <input value={firstName} onChange={(e) => setFirstName(e.target.value)} type="text" className="form-control" id="firstName" placeholder="" required />
-                                    <div className="invalid-feedback">
-                                        Valid first name is required.
-                            </div>
+                                    <input value={firstName} onChange={first} type="text" className="form-control" id="firstName" placeholder="" required />
+                                    <p style={{ marginTop: '10px', color: 'red' }}>{nameError}</p>
+
                                 </div>
                                 <div className="col-md-6 mb-3">
                                     <label htmlFor="lastName">Last name</label>
-                                    <input value={lastName} onChange={(e) => setLastName(e.target.value)} type="text" className="form-control" id="lastName" placeholder="" required />
-                                    <div className="invalid-feedback">
-                                        Valid last name is required.
-                                </div>
+                                    <input value={lastName} onChange={last} type="text" className="form-control" id="lastName" placeholder="" required />
+                                    <p style={{ marginTop: '10px', color: 'red' }}>{nameError}</p>
+
                                 </div>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="email">Number <span className="text-muted"></span></label>
-                                <input value={number} onChange={(e) => setNumber(e.target.value)} type="number" className="form-control" id="email" required placeholder="Enter Your Mobile Number" />
-                                <div className="invalid-feedback">
-                                    Please enter a valid Number for shipping updates.
-                            </div>
+                                <input value={number} min="11" onChange={(e) => setNumber(e.target.value)} type="number" className="form-control" id="email" placeholder="Enter Your Mobile Number" required />
+                                <p style={{ marginTop: '10px', color: 'red' }}>{numberError}</p>
                             </div>
 
                             <div className="mb-3">
                                 <label htmlFor="email">Email <span className="text-muted"></span></label>
-                                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" className="form-control" id="email" required placeholder="you@example.com" />
-                                <div className="invalid-feedback">
-                                    Please enter a valid email address for shipping updates.
-                            </div>
+                                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" className="form-control" id="email" placeholder="you@example.com" required />
                             </div>
 
                             <div className="mb-3">
                                 <label htmlFor="address">Address</label>
                                 <input value={address} onChange={(e) => setAddress(e.target.value)} type="text" className="form-control" id="address" placeholder="1234 Main St" required />
-                                <div className="invalid-feedback">
-                                    Please enter your shipping address.
-        </div>
                             </div>
 
                             <hr className="mb-4" />
@@ -114,6 +168,7 @@ const CheckoutRight = () => {
                                     <input id="paypal" onClick={() => setPayment('Cash On Delivery')} name="paymentMethod" type="radio" className="custom-control-input" required />
                                     <label className="custom-control-label" htmlFor="paypal">COD</label>
                                 </div>
+                                <p>{paymentError}</p>
                                 <div>
 
                                 </div>
@@ -121,41 +176,23 @@ const CheckoutRight = () => {
                             </div>
 
                             <div class="form-check">
-                                <input onClick={() => setOrderType('Order')} class="form-check-input" type="checkbox" id="flexCheckDefault" />
+                                <input name="checkbox" onClick={() => setOrderType('Order')} class="form-check-input" type="checkbox" id="flexCheckChecked" />
                                 <label class="form-check-label" for="flexCheckDefault">
                                     Order
-                          </label>
+                        </label>
                             </div>
                             <div class="form-check">
-                                <input onClick={() => setOrderType('Subscription')} class="form-check-input" type="checkbox" id="flexCheckChecked" />
+                                <input name="checkbox" onClick={() => setOrderType('Subscription')} class="form-check-input" type="checkbox" id="flexCheckChecked" />
                                 <label class="form-check-label" for="flexCheckChecked">
                                     Subscription
                         </label>
+                                <p>{orderTypeError}</p>
                             </div>
-
-                            {/* <div className="row">
-                                <div className="col-md-6 mb-3">
-                                    <label htmlFor="cc-name">Name on card</label>
-                                    <input type="text" className="form-control" id="cc-name" placeholder="" required />
-                                    <small className="text-muted">Full name as displayed on card</small>
-                                    <div className="invalid-feedback">
-                                        Name on card is required
-                               </div>
-                                </div>
-
-                                <div className="col-md-6 mb-3">
-                                    <label htmlFor="cc-number">Credit card number</label>
-                                    <input type="text" className="form-control" id="cc-number" placeholder="" required />
-                                    <div className="invalid-feedback">
-                                        Credit card number is required
-            </div>
-                                </div>
-                            </div> */}
 
                             <hr className="mb-4" />
 
                             {user ? (
-                                <button onClick={save} className="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>) :
+                                <button type="submit" className="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>) :
                                 <button onClick={() => alert('Sign In To Proceed')} className="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>
                             }
                             {/* */}
